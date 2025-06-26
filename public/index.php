446af4e96484d90c27415ef454c8eb10ea91a0ab
Caio Cesar de Deus <caioc.deus@outlook.com>
2025-06-25 22:31:03 -0300

    Alteração index.php

--- public/index.php
+++ public/index.php
@@ -3,15 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
